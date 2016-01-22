6bc8709af23831c22d5b770562c4cde7ce8b2ff8
ag <aaron@urbankayaks.com>
2016-01-22 14:13:59 -0600

    add spec for replaceNumber function

--- specs/specs.js
+++ specs/specs.js
@@ -1,7 +1,15 @@
 
 describe("listNumbers", function(){
 
-  it("takes an input and returns and returns a range of numbers from 1 to the users input and place into array", function(){
+  it("takes an input and returns a range of numbers from 1 to the users input and place into array", function(){
     expect(listNumbers(5)).to.equal[1,2,3,4,5];
+  });
+});
+
+
+describe("replaceNumbers", function(){
+
+  it("looks through array and replaces all values divisible by 3,5,15 with their corrosponding string and puts them into a list", function(){
+    expect(listNumbers(5)).to.equal("<li>1</li><li>2</li><li><Ping/li><li>4</li><li>Pong</li>");
   });
 });
