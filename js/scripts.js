bfa141d869f71f5331a1ebb5df74e97283e5de2a
ag <aaron@urbankayaks.com>
2016-01-22 14:08:51 -0600

    add first spec which takes in user input and returns 1 through user input and places into array

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,8 @@
-var pingpong = function (number){
-  for (var i = 0, i <= number, i = i +1);
+var listNumbers = function(input){
+  var numbers = []
+  for (var i = 1; i <= input; i = i +1){
+    numbers.push(i)
+  }
+
+return numbers
 }
--- specs/specs.js
+++ specs/specs.js
@@ -1,7 +1,7 @@
 
-describe("pingppong", function(){
+describe("listNumbers", function(){
 
-  it("takes an input and returns and returns a range of numbers from 1 to the users input", function(){
-    expect(pingpong(10)).to.equal(1,2,3,4,5,6,7,8,9);
+  it("takes an input and returns and returns a range of numbers from 1 to the users input and place into array", function(){
+    expect(listNumbers(5)).to.equal[1,2,3,4,5];
   });
 });
