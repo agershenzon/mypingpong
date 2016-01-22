26e2020f7e557dc8502a5ba11efff9e74ffcbf1e
ag <aaron@urbankayaks.com>
2016-01-22 13:51:28 -0600

    intiial test seeting up folder

--- specs/specs.js
+++ specs/specs.js
@@ -1,22 +1,7 @@
-describe("translator", function(){
 
-  it("check to see if the word starts with a vowel. if true, add 'ay' to the end", function(){
-    expect(translator("apple")).to.equal("appleay");
-  });
+describe("pingppong", function(){
 
-  it("check to see if the word starts with a 'qu'. if true, move to end and add 'ay'", function(){
-    expect(translator("quilt")).to.equal("iltquay");
-  });
-
-  it("check to see if the word starts with a 'squ'. if true, move to end and add 'ay'", function(){
-    expect(translator("squeal")).to.equal("ealsquay");
-  });
-
-  it("check to see if it starts with a consonant. if true, move consecutive consonants to end until you hit a vowel and add 'ay'", function(){
-    expect(translator("llama")).to.equal("amallay");
-  });
-
-  it("takes a sentence and splits each word into a string and runs all functions above", function(){
-    expect(translator("good luck")).to.equal("oodgay ucklay");
+  it("takes an input and returns and returns a range of numbers from 1 to the users input", function(){
+    expect(pingpong(10)).to.equal(1,2,3,4,5,6,7,8,9);
   });
 });
